2cecb89b3b467686e98a92617ae5f80b8ead6f40
Jonathan <jonathannsegal@gmail.com>
2019-10-19 22:23:48 -0500

    added objects

---
+++ Assets/MainGame/Scripts/WindmillPropeller.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindmillPropeller : MonoBehaviour
+{
+    public float speed;
+    public Vector3 rotationVector;
+
+    void Update()
+    {
+        transform.Rotate(rotationVector, speed * Time.deltaTime);
+    }
+}
+
+
